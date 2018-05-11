55d513a7b7910f9e6a1179ebf053f1c16b821e3d
AlexanderGri <grishin@phystech.edu>
2018-05-11 16:26:56 +0300

    Migration to pytorch 0.4

--- bayeasy.py
+++ bayeasy.py
@@ -1,74 +1,64 @@
 import torch
-from torch.autograd import Variable as Var
 from torch import nn
+from torch.distributions.normal import Normal
 from types import MethodType
 import numpy as np
 
 
-prefixes = ['mu_', 'logsigma_']
-log2pi = float(np.log(2 * np.pi))
+DEBUG_INFO = True
 
-def log_normal(x, mean, log_std):
-    return - 0.5 * log2pi - log_std - (x - mean) ** 2 / (2 * torch.exp(2 * log_std))
+param_prefixes = ['_prior_loc_', '_prior_scale_', '_posterior_loc_', '_posterior_scale_']
 
-def is_valid_parameter(l, key):
+def is_variational_parameter(l, key):
 
-    if key in l.variational_parameters:
-        return False
+    if key in l._variational_parameters:
+        return True
 
-    for prefix in prefixes:
-        if key.startswith(prefix) and key[len(prefix):] in l.variational_parameters:
-            return False
+    for prefix in param_prefixes:
+        if key.startswith(prefix) and key[len(prefix):] in l._variational_parameters:
+            return True
         
-    return True
+    return False
 
 def bayesify_parameter(l, key):
-    if not hasattr(l, 'variational_parameters'):
-        l.variational_parameters = []
-        
-    if not is_valid_parameter(l, key):
+    if is_variational_parameter(l, key):
         return
     
     value = getattr(l, key)
     shape = value.data.shape
-    l.register_buffer('sample_for_' + key, nn.Parameter(torch.randn(*shape)))
-
-    l.register_buffer('prior_mu_' + key, nn.Parameter(torch.zeros(*shape)))
-    l.register_buffer('prior_logsigma_' + key, nn.Parameter(torch.zeros(*shape)))
-
-    l.register_parameter('mu_' + key, nn.Parameter(torch.randn(*shape)))
-    l.register_parameter('logsigma_' + key, nn.Parameter(torch.randn(*shape)))
     
-    l.variational_parameters.append(key)
-    
-    print('Parameter %s in %s is bayesified' % (key, str(l)))
+    loc, scale = (0., 1.)
+    l.register_buffer('_prior_loc_%s' % key, loc * torch.ones(*shape))
+    l.register_buffer('_prior_scale_%s' % key, scale * torch.ones(*shape))
+    l._distributions['prior_%s' % key] = Normal(l._buffers['_prior_loc_%s' % key],
+                                                l._buffers['_prior_scale_%s' % key])
+
+    l.register_parameter('_posterior_loc_%s' % key, nn.Parameter(loc * torch.ones(*shape, requires_grad=True)))
+    l.register_parameter('_posterior_scale_%s' % key, nn.Parameter(scale * torch.ones(*shape, requires_grad=True)))
+    l._distributions['posterior_%s' % key] = Normal(l._parameters['_posterior_loc_%s' % key],
+                                                    l._parameters['_posterior_scale_%s' % key])
+
+    l._variational_parameters.append(key)
+
+    if DEBUG_INFO: print('Parameter %s in %s is bayesified' % (key, str(l)))
 
 def sampling_hook(m, i):
-    for key in m.variational_parameters:
-        print('Sampling ' + key)
-        name = 'sample_for_' + key
-        sample = Var(torch.randn(*m._buffers[name].data.shape))
-        m._buffers[name] = sample
-        mu = getattr(m, 'mu_' + key)
-        logsigma = getattr(m, 'logsigma_' + key)
-        m._parameters[key] = mu + torch.exp(logsigma) * sample
+    for key in m._variational_parameters:
+        if DEBUG_INFO: print('Sampling ' + key)
+        m._parameters[key] = m._distributions['posterior_%s' % key].rsample()
         
-def get_var_cost(model):
-    var_cost = Var(torch.zeros(1), requires_grad=True)
+def get_var_cost(m):
+    # Regularizer and noise according to Sctochastic Gradient Langevin Dynamics
+
+    var_cost = 2 * torch.randn(1) # noise
     
-    if hasattr(model, 'variational_parameters'):    
-        for key in model.variational_parameters:
-            sample = getattr(model, key)
+    if hasattr(m, '_variational_parameters'):    
+        for key in m._variational_parameters:
+            sample = m._parameters[key]
+            prior_distr = m._distributions['prior_%s' % key]
+            var_cost = var_cost + prior_distr.log_prob(sample).sum() # regularizer
 
-            prior_mu = getattr(model, 'prior_mu_' + key)
-            mu = getattr(model, 'mu_' + key)
-
-            prior_logsigma = getattr(model, 'prior_logsigma_' + key)
-            logsigma = getattr(model, 'logsigma_' + key)
-
-            var_cost = var_cost + torch.sum(log_normal(sample, mu, logsigma) - log_normal(sample, prior_mu, prior_logsigma))
-
-    for child in model.children():
+    for child in m.children():
         if hasattr(child, 'get_var_cost'):
             var_cost = var_cost + get_var_cost(child)
     
@@ -77,10 +67,15 @@ def get_var_cost(model):
 def bayesify(l):
     keys   = []
     shapes = []
+    if not hasattr(l, "_variational_parameters"):
+        l._variational_parameters = []
+        l._distributions = {}
+        l.register_forward_pre_hook(sampling_hook)
+        l.get_var_cost = MethodType(get_var_cost, l)
+
     for key in list(l._parameters.keys()):
-        bayesify_parameter(l, key)
-    l.register_forward_pre_hook(sampling_hook)
-    l.get_var_cost = MethodType(get_var_cost, l)
-    
+        if not is_variational_parameter(l, key):
+            bayesify_parameter(l, key)
+
     for child in l.children():
         bayesify(child)
